6a407201d169765f5186924bd28a146bac8b15b8
Goychak M <musayevagoychak2@gmail.com>
2025-02-06 18:29:12 +0400

    v1_goychak

--- src/test/java/myapp/pages/AlloverCommerce_StoreManagerPage.java
+++ src/test/java/myapp/pages/AlloverCommerce_StoreManagerPage.java
@@ -1,10 +1,18 @@
 package myapp.pages;
 
 import myapp.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class AlloverCommerce_StoreManagerPage {
     public AlloverCommerce_StoreManagerPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy(partialLinkText = "Products")
+    public WebElement productsMenuTab;
+
+    @FindBy(partialLinkText = "Add New")
+    public WebElement addNew;
 }
